3e09b4ee0835d31e310ecbdf61bdb779f0213a5f
tawhidul2122 <tawhidul2122@gmail.com>
2022-09-18 01:12:47 +0600

    800

---
+++ Beautiful-Matrix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int x = 0;
+    for (int i = 1; i <= 5; ++i)
+    {
+        for (int j = 1; j <= 5; ++j)
+        {
+            cin >> x;
+            if (x == 1)
+            {
+                cout << fabs(i - 3) + fabs(j - 3) << endl;
+            }
+        }
+    }
+    return 0;
+}
